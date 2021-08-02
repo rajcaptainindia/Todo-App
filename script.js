daba0bb73786b43bf2ddca9c065760fbbba2ef0f
rajcaptainindia <rajcsecr@gmail.com>
2021-08-02 13:27:04 +0530

    Fixed Data Error / Data is Displaying
    Previously the data is not displayed after clicking on add button due to an error (at appendchild(), id is not mentioned)
    now functionality is working fine.

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var newData = document.getElementById('input1');
 var ar =[];
 function addData(){
     ar.push(newData.value);
-    console.log(ar);
-    newData.value="";
+    console.log(ar[ar.length-1]);
     let datacontainer = document.getElementById("data-container");
-    let output = document.createElement('H1');
-    output.textContent="Rajkumar"
-    output.classList.add('data');
+    let output = document.createElement('p');
+   // output.textContent=ar[0];
+    output.textContent=ar[ar.length-1];
+   output.classList.add('data');
     datacontainer.appendChild(output);
-    
+    newData.textContent="";
+        //newData.value="";
 };
 
